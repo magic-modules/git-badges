cb05950c037b13ecd2059b985d1cac334259fbda
Jascha <jascha@jaeh.at>
2019-04-21 22:19:18 +0200

    format test

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,4 @@
 const { is } = require('@magic/test')
 const GitBadges = require('../src')
 
-module.exports = [
-  { fn: () => GitBadges, expect: is.function, info: 'GitBadges is a function' },
-]
+module.exports = [{ fn: () => GitBadges, expect: is.function, info: 'GitBadges is a function' }]
