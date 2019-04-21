50baf5491db67bc55cbe2311498f9623d3ef5bc7
Jascha <jascha@jaeh.at>
2019-04-21 22:20:54 +0200

    rebuild, format

--- src/index.js
+++ src/index.js
@@ -1,16 +1,58 @@
-const GitBadges = ({ npm, travis, appveyor, coveralls, greenkeeper, branch = 'master' }) =>
-  div({ class: `Badges` }, [
-    npm && Link({ to: `https://www.npmjs.com/package/@${npm}`, }, Img({ src: `https://img.shields.io/npm/v/@${npm}.svg` })),
-    travis && Link({ to: `https://travis-ci.com/${travis}` }, Img({ src: `https://travis-ci.com/${travis}.svg?branch=${branch}` })),
-    appveyor && Link({ to: `https://ci.appveyor.com/project/${appveyor}/branch/${branch}` }, Img({ src: `https://img.shields.io/appveyor/ci/${appveyor}/${branch}.svg` })),
-    coveralls && Link({ to: `https://coveralls.io/github/${coveralls}` }, Img({ src: `https://coveralls.io/repos/github/${coveralls}/badge.svg` })),
-    greenkeeper && Link({ to: `https://greenkeeper.io` }, Img({ src: `https://badges.greenkeeper.io/${greenkeeper}.svg` })),
+const GitBadges = props => {
+  const { project = false } = props
+  const {
+    npm = project,
+    travis = project,
+    appveyor = project,
+    coveralls = project,
+    greenkeeper = project,
+    branch = 'master',
+  } = props
+
+  // show nothing if no badges could be shown anyways
+  if (!project && !npm && !travis && !appveyor && !coveralls && !greenkeeper) {
+    return
+  }
+
+  const GitBadge = ({ to, src }) => li([Link({ to }, Img({ src: src }))])
+
+  return ul({ class: 'GitBadges' }, [
+    npm &&
+      GitBadge({
+        to: `https://www.npmjs.com/package/@${npm}`,
+        src: `https://img.shields.io/npm/v/@${npm}.svg`,
+      }),
+    travis &&
+      GitBadge({
+        to: `https://travis-ci.com/${travis}`,
+        src: `https://travis-ci.com/${travis}.svg?branch=${branch}`,
+      }),
+    appveyor &&
+      GitBadge({
+        to: `https://ci.appveyor.com/project/${appveyor}/branch/${branch}`,
+        src: `https://img.shields.io/appveyor/ci/${appveyor}/${branch}.svg`,
+      }),
+    coveralls &&
+      GitBadge({
+        to: `https://coveralls.io/github/${coveralls}`,
+        src: `https://coveralls.io/repos/github/${coveralls}/badge.svg`,
+      }),
+    greenkeeper &&
+      GitBadge({
+        to: `https://greenkeeper.io`,
+        src: `https://badges.greenkeeper.io/${greenkeeper}.svg`,
+      }),
   ])
+}
 
 GitBadges.style = {
-  '.Badges': {
-    'a': {
+  '.GitBadges': {
+    display: 'inline-block',
+
+    li: {
       margin: `0 .5em 0 0`,
+      display: 'inline-block',
+      float: 'left',
     },
   },
 }
