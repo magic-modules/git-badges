a811cf75a21724cdc7ed22c37925e87f1857b312
Jascha <jascha@jaeh.at>
2019-05-03 08:39:27 +0200

    use array instead of object

--- src/index.js
+++ src/index.js
@@ -9,43 +9,63 @@ const GitBadges = props => {
 
   const { project = false, branch = 'master' } = props
 
-  const urls = Object.entries({
-    npm: (v = project) =>
-      v && {
-        to: `https://www.npmjs.com/package/@${v}`,
-        src: `https://img.shields.io/npm/v/@${v}.svg`,
-      },
-    travis: (v = project) =>
-      v && {
-        to: `https://travis-ci.com/${v}`,
-        src: `https://travis-ci.com/${v}.svg?branch=${branch}`,
-      },
-    appveyor: (v = project) => {
-      if (v) {
-        let [org, repo] = v.split('/')
-        org = org.replace(/-/g, '')
-        return {
-          to: `https://ci.appveyor.com/project/${org}/${repo}/branch/${branch}`,
-          src: `https://img.shields.io/appveyor/ci/${org}/${repo}/${branch}.svg`,
+  // this pattern allows capture of props that are set to false and intended to hide badges.
+  // it will also make every undefined prop[serviceName] default to project
+  const urls = [
+    [
+      'npm',
+      (v = project) =>
+        v && {
+          to: `https://www.npmjs.com/package/@${v}`,
+          src: `https://img.shields.io/npm/v/@${v}.svg`,
+        },
+    ],
+    [
+      'travis',
+      (v = project) =>
+        v && {
+          to: `https://travis-ci.com/${v}`,
+          src: `https://travis-ci.com/${v}.svg?branch=${branch}`,
+        },
+    ],
+    [
+      'appveyor',
+      (v = project) => {
+        if (v) {
+          let [org, repo] = v.split('/')
+          org = org.replace(/-/g, '')
+          return {
+            to: `https://ci.appveyor.com/project/${org}/${repo}/branch/${branch}`,
+            src: `https://img.shields.io/appveyor/ci/${org}/${repo}/${branch}.svg`,
+          }
         }
-      }
-    },
-    coveralls: (v = project) =>
-      v && {
-        to: `https://coveralls.io/github/${v}`,
-        src: `https://coveralls.io/repos/github/${v}/badge.svg`,
       },
-    greenkeeper: (v = project) =>
-      v && {
-        to: `https://greenkeeper.io`,
-        src: `https://badges.greenkeeper.io/${v}.svg`,
-      },
-    snyk: (v = project) =>
-      v && {
-        to: `https://snyk.io/test/github/${v}`,
-        src: `https://snyk.io/test/github/${v}/badge.svg`,
-      },
-  })
+    ],
+    [
+      'coveralls',
+      (v = project) =>
+        v && {
+          to: `https://coveralls.io/github/${v}`,
+          src: `https://coveralls.io/repos/github/${v}/badge.svg`,
+        },
+    ],
+    [
+      'greenkeeper',
+      (v = project) =>
+        v && {
+          to: `https://greenkeeper.io`,
+          src: `https://badges.greenkeeper.io/${v}.svg`,
+        },
+    ],
+    [
+      'snyk',
+      (v = project) =>
+        v && {
+          to: `https://snyk.io/test/github/${v}`,
+          src: `https://snyk.io/test/github/${v}/badge.svg`,
+        },
+    ],
+  ]
     .map(([name, fn]) => fn(props[name]))
     .filter(a => a.to && a.src)
 
