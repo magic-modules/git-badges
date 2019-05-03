42ee9d93839525e208be2513e81af4b8d473501b
Jascha <jascha@jaeh.at>
2019-05-03 07:29:20 +0200

    format, use string in demo

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,33 +1,33 @@
 module.exports = state => [
-    h1(state.title),
-    p([
-      'this is the ',
-      Link({ to: 'https://github.com/magic-modules' }, '@magic-modules'),
-      ' GitBadges component. ',
-      state.description,
-    ]),
+  h1(state.title),
+  p([
+    'this is the ',
+    Link({ to: 'https://github.com/magic-modules' }, '@magic-modules'),
+    ' GitBadges component. ',
+    state.description,
+  ]),
 
-    h2({ id: 'installation' }, 'installation'),
-    p(
-      'installation is done using npm. for now, all magic modules are living on github and not on npm.',
-    ),
-    p('note the missing @ before magic-modules.'),
-    p('this is how we install npm modules from github.'),
-    Pre(`npm install magic-modules/git-badges`),
+  h2({ id: 'installation' }, 'installation'),
+  p(
+    'installation is done using npm. for now, all magic modules are living on github and not on npm.',
+  ),
+  p('note the missing @ before magic-modules.'),
+  p('this is how we install npm modules from github.'),
+  Pre(`npm install magic-modules/git-badges`),
 
-    h2({ id: 'require' }, 'require'),
-    p('first add the component to the assets'),
-    Pre(`
+  h2({ id: 'require' }, 'require'),
+  p('first add the component to the assets'),
+  Pre(`
 // assets/index.js
 module.exports = {
   //...other exports
   GitBadges: require('@magic-modules/git-badges'),
 }`),
-    h2({ id: 'usage' }, 'usage'),
-    p(
-      'in a page/component, just pass the badge user/project string for those badges you want to show:',
-    ),
-    Pre(`
+  h2({ id: 'usage' }, 'usage'),
+  p(
+    'in a page/component, just pass the badge user/project string for those badges you want to show:',
+  ),
+  Pre(`
 GitBadges({
   // show all badges,
   // insert project into the various urls of the badges
@@ -45,8 +45,9 @@ GitBadges({
   branch: 'master',
 })
 `),
-    p('renders'),
-    GitBadges({
-      project: 'magic-modules/git-badges',
-    }),
-  ]
+
+  h2({ id: 'demo' }, 'demo'),
+  Pre("GitBadges('magic-modules/git-badges')"),
+  p('renders'),
+  GitBadges('magic-modules/git-badges'),
+]
